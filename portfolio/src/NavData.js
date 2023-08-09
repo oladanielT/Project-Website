4fa1ba18a441396cdc37bd043d92ccf74ae716b0
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-08-09 04:56:17 +0100

    Added the Navigation Menu and the social links button

---
+++ portfolio/src/NavData.js
@@ -0,0 +1,33 @@
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai';
+
+
+const NavData =[
+
+    {
+        icon:<AiFillLinkedin size={30}/>,
+        title:'LinkedIn',
+        bg:'#4299e1',
+    },
+    
+    {
+        icon:<AiFillGithub size={30}/>,
+        title:'Github',
+        bg:'#333333',
+    },
+    
+    {
+        icon:<AiOutlineMail size={30}/>,
+        title:'Email',
+        bg:'#6fc2b0',
+    },
+    
+    {
+        icon:<BsFillPersonLinesFill size={30}/>,
+        title:'Resume',
+        bg:'#565f69',
+    }
+]
+
+
+export default NavData;
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React , {useState} from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
+import Socials from '../common/Socials';
+
+
+
+function Navbar(){
+    const [show, setShow] = useState(true);
+
+
+    return(
+        <div className='fixed w-full h-[80px] flex items-center justify-between bg-[#0a192f] text-gray-400 px-6'>
+            <div>
+                <img src="" alt="logo" />
+            </div>
+            <div className=''>
+                <ul className='hidden md:flex items-center font-bold'>
+                    <li className='mx-3'>Home</li>
+                    <li className='mx-3'>About</li>
+                    <li className='mx-3'>Skills</li>
+                    <li className='mx-3'>Works</li>
+                    <li className='mx-3'>Contact</li>
+                </ul>
+            </div>
+            <div className='block md:hidden' onClick={()=>setShow(!show)}>
+                {
+                    show?
+                    <AiOutlineClose size={30}/>
+                    :
+                    <AiOutlineMenu size={30}/>
+                }
+            </div>
+          
+            <div className={` fixed ${show?"left-0 ":'left-[-100%]'} top-[82px] w-full h-screen flex flex-col justify-center text-3xl items-center bg-[#0a192f] py-4 md:hidden ease-in-out duration-300 transition-all z-10`} >
+                <ul className='flex flex-col justify-center font-bold'>
+                    <li className='p-4 cursor-pointer'>Home</li>
+                    <li className='p-4 cursor-pointer'>About</li>
+                    <li className='p-4 cursor-pointer'>Skills</li>
+                    <li className='p-4 cursor-pointer'>Works</li>
+                    <li className='p-4 cursor-pointer'>Contact</li>
+                </ul>
+            </div>
+            <Socials/>
+
+            
+
+        </div>
+    )
+}
+
+export default Navbar;
